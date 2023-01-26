b3b36131629e2f0811bad71fe57e5c5097e89614
gosidominik <gosidominik@gmail.com>
2023-01-26 11:54:10 +0100

    Initial commit

---
+++ src/main/java/org/example/TollSystemStorage.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.entity.MotorwayVignette;
+import org.example.interfaces.TollSystemStorageInterface;
+
+public class TollSystemStorage implements TollSystemStorageInterface {
+    @Override
+    public MotorwayVignette getVignetteByVehicleRegistration(String s) {
+        return null;
+    }
+}
